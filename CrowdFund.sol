67e5c21f8b2e7750da428aa98be8c1f42d17994b
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-22 14:00:30 +0800

    Initial commit

---
+++ CrowdFund.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+contract CrowdFund {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor (uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
+    
+    function createRequest() public restricted {
+        
+    }
+}
