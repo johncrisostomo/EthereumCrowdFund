108f155fadfbd77ad9c2507b007091cf89289c52
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-23 15:42:38 +0800

    Implements finalize request

--- CrowdFund.sol
+++ CrowdFund.sol
@@ -14,6 +14,7 @@ contract CrowdFund {
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
+    uint public approversCount;
     
     modifier restricted() {
         require(msg.sender == manager);
@@ -29,6 +30,8 @@ contract CrowdFund {
         require(msg.value > minimumContribution);
         
         approvers[msg.sender] = true;
+        
+        approversCount++;
     }
     
     function createRequest(string description, uint value, address recipient) 
@@ -53,5 +56,16 @@ contract CrowdFund {
         
         request.approvals[msg.sender] = true;
         request.approvalCount++;
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        
+        require(request.approvalCount > (approversCount/ 2));
+        require(request.complete);
+
+        request.recipient.transfer(request.value);
+        
+        request.complete = true;
     }
 }
