5ad960d0402c85e035757482603b75cfcda78541
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-23 19:57:49 +0800

    Add first test, just to make sure it is getting deployed

--- test/CrowdFund.test.js
+++ test/CrowdFund.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider);
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const compiledFactory = require('../ethereum/build/CrowdFundFactory');
 const compiledCrowdFund = require('../ethereum/build/CrowdFund');
@@ -14,8 +15,7 @@ let crowdFund;
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    factory = await web3.eth
-        .Contract(JSON.parse(compiledFactory.interface))
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
 
@@ -23,9 +23,17 @@ beforeEach(async () => {
         .createCrowdFund('100')
         .send({ from: accounts[0], gas: '1000000' });
 
-    [crowdFundAddress] = await factory.methods.getDeployedCrowdFunds.call();
+    [crowdFundAddress] = await factory.methods.getDeployedCrowdFunds().call();
 
-    crowdFund = await web3.eth.Contract(
-        JSON.parse(compiledCrowdFund.interface, crowdFundAddress)
+    crowdFund = await new web3.eth.Contract(
+        JSON.parse(compiledCrowdFund.interface),
+        crowdFundAddress
     );
+});
+
+describe('CrowdFunds', () => {
+    it('deploys a factory and a crowd fund', async () => {
+        assert.ok(factory.options.address);
+        assert.ok(crowdFund.options.address);
+    });
 });
