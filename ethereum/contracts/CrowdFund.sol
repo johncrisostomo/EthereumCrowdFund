09bdbf7c526c939793697e018350d897fd23a2df
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 17:05:37 +0800

    Add helper methods for the view

--- ethereum/contracts/CrowdFund.sol
+++ ethereum/contracts/CrowdFund.sol
@@ -82,4 +82,18 @@ contract CrowdFund {
         
         request.complete = true;
     }
+
+    function getSummary() public view returns (uint, uint, uint, uint, address){
+        return (
+            minimumContribution,
+            address(this).balance, 
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
 }
