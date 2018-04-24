1112a32d43fbdbeac83fbf9c6d7ab9ecbd3be463
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 15:25:27 +0800

    Sets up CrowdFundView and route

---
+++ pages/crowdfunds/view.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CrowdFundView extends Component {
+    render() {
+        return <h3>CrowdFund View</h3>;
+    }
+}
+
+export default CrowdFundView;
