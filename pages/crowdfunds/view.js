87938d4396e281566664609df1b1de9bb92dd00b
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 18:20:23 +0800

    Add cards to show CF summary

--- pages/crowdfunds/view.js
+++ pages/crowdfunds/view.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
+
 import Layout from '../../components/layout';
+
+import web3 from '../../ethereum/web3';
 import crowdFundHelper from '../../ethereum/crowdfund';
 
 class CrowdFundView extends Component {
@@ -16,10 +20,56 @@ class CrowdFundView extends Component {
         };
     }
 
+    renderCards = () => {
+        const {
+            minimumContribution,
+            balance,
+            requestsCount,
+            approversCount,
+            manager
+        } = this.props;
+
+        const items = [
+            {
+                header: manager,
+                description:
+                    'The address of the manager who created this CrowdFund and can create requisition requests',
+                meta: 'Address of CF Manager',
+                style: { overflowWrap: 'break-word' }
+            },
+            {
+                header: minimumContribution,
+                description:
+                    'Minimum amount you can contribute to become an approver',
+                meta: 'Minimum Contribution (wei)'
+            },
+            {
+                header: requestsCount,
+                description:
+                    'A requisition request for buying stuff and sending a certain amount to a vendor',
+                meta: 'Number of Requests'
+            },
+            {
+                header: approversCount,
+                description:
+                    'Number of people who have already contributed and can approve requests',
+                meta: 'Number of Approvers'
+            },
+            {
+                header: web3.utils.fromWei(balance, 'ether'),
+                description: 'Current balance of this CrowdFund',
+                meta: 'CrowdFund Balance (ETH)'
+            }
+        ];
+
+        return <Card.Group items={items} />;
+    };
+
     render() {
         return (
             <Layout>
                 <h3>CrowdFund Details</h3>
+                {this.renderCards()}
             </Layout>
         );
     }
