7735a62b489333879a191d5691e1dd7328fd0ea4
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 18:06:19 +0800

    Prepares data

--- pages/crowdfunds/view.js
+++ pages/crowdfunds/view.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import Layout from '../../components/layout';
+import crowdFundHelper from '../../ethereum/crowdfund';
 
 class CrowdFundView extends Component {
+    static async getInitialProps(props) {
+        const crowdfund = crowdFundHelper(props.query.address);
+        const summary = await crowdfund.methods.getSummary().call();
+
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
+    }
+
     render() {
-        return <h3>CrowdFund View</h3>;
+        return (
+            <Layout>
+                <h3>CrowdFund Details</h3>
+            </Layout>
+        );
     }
 }
 
