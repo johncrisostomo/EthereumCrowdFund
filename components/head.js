0d631190b8dc209c3a8d20a68c047f0b544674f1
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 18:04:37 +0800

    Add title

--- components/head.js
+++ components/head.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 export default () => (
     <div>
         <Head>
+            <title>Ethereum CrowdFunder</title>
             <link
                 rel="stylesheet"
                 href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
