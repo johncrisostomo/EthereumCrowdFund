f449092bc8c7c847f7457e6bc67fb4c3d4c01557
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 23:13:08 +0800

    Implements finalize request

--- components/requestrow.js
+++ components/requestrow.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
 
+import crowdfundHelper from '../ethereum/crowdfund';
+
 class RequestRow extends Component {
+    onApprove = async () => {
+        const { id, address } = this.props;
+        const accounts = await web3.eth.getAccounts();
+
+        const crowdfund = crowdfundHelper(address);
+
+        await crowdfund.methods.approveRequest(id).send({
+            from: accounts[0]
+        });
+    };
+
+    onFinalize = async () => {
+        const { id, address } = this.props;
+        const accounts = await web3.eth.getAccounts();
+
+        const crowdfund = crowdfundHelper(address);
+
+        await crowdfund.methods.finalizeRequest(id).send({
+            from: accounts[0]
+        });
+    };
+
     render() {
         const { Row, Cell } = Table;
 
@@ -21,8 +45,16 @@ class RequestRow extends Component {
                 <Cell>
                     {approvalCount} / {approversCount}
                 </Cell>
-                <Cell>Click me</Cell>
-                <Cell>Click me</Cell>
+                <Cell>
+                    <Button color="green" basic onClick={this.onApprove}>
+                        Approve
+                    </Button>
+                </Cell>
+                <Cell>
+                    <Button color="teal" basic onClick={this.onFinalize}>
+                        Finalize
+                    </Button>
+                </Cell>
             </Row>
         );
     }
