9ff14a1dea6f119ea00fbe4cce305e0c6a3c2f44
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 10:31:30 +0800

    Add conditional to handle SSR

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,3 +1,17 @@
 import Web3 from 'web3';
+import { infuraApiKey } from './settings.json';
 
-export default new Web3(window.web3.currentProvider);
+let web3;
+const network = 'rinkeby';
+
+if (typeof window !== 'undefined' && window.web3 !== 'undefined') {
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        `https://${network}.infura.io/${infuraApiKey}`
+    );
+
+    web3 = new Web3(provider);
+}
+
+export default web3;
