9153001a3651be182e020651c9cd0d1106a5c2f4
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 21:19:27 +0800

    Create the form

--- pages/crowdfunds/requests/new.js
+++ pages/crowdfunds/requests/new.js
@@ -1,8 +1,63 @@
 import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+
+import crowdfundHelper from '../../../ethereum/crowdfund';
+import web3 from '../../../ethereum/web3';
+
+import Layout from '../../../components/layout';
 
 class RequestsNew extends Component {
+    state = { value: '', description: '', recipient: '' };
+    static async getInitialProps(props) {
+        const { address } = props.query;
+        return { address };
+    }
+
     render() {
-        return <h3>Create a request</h3>;
+        return (
+            <Layout>
+                <h3>Create a request</h3>
+                <Form>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event =>
+                                this.setState({
+                                    description: event.target.value
+                                })
+                            }
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Value in ETH</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event =>
+                                this.setState({
+                                    value: event.target.value
+                                })
+                            }
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Recipient</label>
+                        <Input
+                            value={this.state.recipient}
+                            onChange={event =>
+                                this.setState({
+                                    recipient: event.target.value
+                                })
+                            }
+                        />
+                    </Form.Field>
+                    <Button primary>Create</Button>
+                </Form>
+            </Layout>
+        );
     }
 }
 
