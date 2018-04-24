4968ea9cb24854409518254e06ff94c36560d07c
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-24 13:27:43 +0800

    Stubs new page

---
+++ pages/crowdfunds/new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../../components/layout';
+
+class CrowdFundsNew extends Component {
+    render() {
+        return (
+            <Layout>
+                <h1>New Crowd Fund</h1>
+            </Layout>
+        );
+    }
+}
+
+export default CrowdFundsNew;
